6576bc9abf5f4391c116aea8c8649e7a852ea212
Nick McGavock <42687502+inspiredProgrammer1921@users.noreply.github.com>
2025-12-08 19:03:20 -0600

    Time: 6 ms (67.55%) | Memory: 64.1 MB (53.36%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+const isPalindrome = function(x) {
+    if (x < 0) {
+        return false;
+    }
+    
+    const str = x.toString();
+    let left = 0;
+    let right = str.length - 1;
+    
+    while (left < right) {
+        if (str[left] !== str[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    
+    return true;
+};
