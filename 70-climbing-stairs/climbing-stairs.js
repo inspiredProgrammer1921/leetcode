3f2253cd388289b3bff6477edd8eb3e46beaf36f
Nick McGavock <42687502+inspiredProgrammer1921@users.noreply.github.com>
2026-01-14 21:22:23 -0600

    Time: 0 ms (100.00%) | Memory: 51.5 MB (99.97%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const climbStairs = function(n) {
+ 
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+    
+    let prev2 = 1; 
+    let prev1 = 2; 
+    
+    for (let i = 3; i <= n; i++) {
+        const current = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = current;
+    }
+    
+    return prev1;
+};
